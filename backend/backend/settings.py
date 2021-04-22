4c92353434dcc0513c62d677ee8faaeae892f8b7
JHowell45 <jhowell45@googlemail.com>
2021-04-22 23:00:50 +0100

    Added app to base site.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'armour_skills.apps.ArmourSkillsConfig'
 ]
 
 MIDDLEWARE = [
