52468b5b3d0a584e5445863e8dac46bcaa3b2dd6
JHowell45 <jhowell45@googlemail.com>
2021-05-09 20:38:07 +0100

    Base Model.

--- backend/armour_skills/models.py
+++ backend/armour_skills/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ArmourSkill(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+    level = models.IntegerField()
