4d43d50be222815e7efc71a4215492dc3367063f
Jacob Howell <jhowell45@googlemail.com>
2021-10-19 19:37:20 +0100

    Creating database model for the armour.

--- backend/backend/armour/models.py
+++ backend/backend/armour/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class ArmourPiece(models.Model):
+    class ArmourType(models.IntegerChoices):
+        HEAD = 0
+        CHEST = 1
+        ARMS = 2
+        WAIST = 3
+        LEGS = 4
+
+    name = models.CharField(max_length=255, unique=True)
+    rarity = models.IntegerField()
+    min_defence = models.IntegerField()
+    max_defence = models.IntegerField()
+    fire_resistance = models.IntegerField()
+    water_resistance = models.IntegerField()
+    thunder_resistance = models.IntegerField()
+    ice_resistance = models.IntegerField()
+    dragon_resistance = models.IntegerField()
+    no_of_one_slot_decorations = models.IntegerField()
+    no_of_two_slot_decorations = models.IntegerField()
+    no_of_three_slot_decorations = models.IntegerField()
+    armour_type = models.IntegerField(ArmourType.choices)
+
+    def no_slots(self) -> bool:
+        return (
+            self.no_of_one_slot_decorations == 0
+            and self.no_of_two_slot_decorations == 0
+            and self.no_of_three_slot_decorations == 0
+        )
