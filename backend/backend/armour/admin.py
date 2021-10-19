41caf105c9abfb32141e0db5399e7400f4b72a9f
Jacob Howell <jhowell45@googlemail.com>
2021-10-19 19:41:28 +0100

    Registering model to the admin page.

--- backend/backend/armour/admin.py
+++ backend/backend/armour/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import ArmourPiece
+
 # Register your models here.
+
+
+@admin.register(ArmourPiece)
+class ArmourPieceAdmin(admin.ModelAdmin):
+    model = ArmourPiece
