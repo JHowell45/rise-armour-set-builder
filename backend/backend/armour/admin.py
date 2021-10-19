8cff03dd007a2e5223cea0620d1d70738ece0584
Jacob Howell <jhowell45@googlemail.com>
2021-10-19 19:56:36 +0100

    Updating what's displayed in the admin page.

--- backend/backend/armour/admin.py
+++ backend/backend/armour/admin.py
@@ -8,3 +8,36 @@ from .models import ArmourPiece
 @admin.register(ArmourPiece)
 class ArmourPieceAdmin(admin.ModelAdmin):
     model = ArmourPiece
+
+    list_display = [
+        "id",
+        "name",
+        "rarity",
+        "min_defence",
+        "max_defence",
+        "fire_resistance",
+        "water_resistance",
+        "thunder_resistance",
+        "ice_resistance",
+        "dragon_resistance",
+        "no_of_one_slot_decorations",
+        "no_of_two_slot_decorations",
+        "no_of_three_slot_decorations",
+    ]
+
+    list_editable = [
+        "name",
+        "rarity",
+        "min_defence",
+        "max_defence",
+        "fire_resistance",
+        "water_resistance",
+        "thunder_resistance",
+        "ice_resistance",
+        "dragon_resistance",
+        "no_of_one_slot_decorations",
+        "no_of_two_slot_decorations",
+        "no_of_three_slot_decorations",
+    ]
+
+    save_on_top = True
