68d306c4f9ec83c056ace07623add6b319dd4563
Jacob Howell <jhowell45@googlemail.com>
2021-10-20 19:18:59 +0100

    Installing cors and setting it up.

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "graphene_django",
+    "corsheaders",
     "armour",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,6 +126,12 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# GraphQL
 GRAPHENE = {
     "SCHEMA": "armour.schema.schema",
 }
+
+
+# Cors Access
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = ("http://localhost:8080",)
