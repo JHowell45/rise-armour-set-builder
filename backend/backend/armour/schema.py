5fe6d89e8ba10970bf4f30557911f732fe0f5cf8
Jacob Howell <jhowell45@googlemail.com>
2021-10-20 19:07:27 +0100

    Updating queries.

--- backend/backend/armour/schema.py
+++ backend/backend/armour/schema.py
@@ -8,3 +8,39 @@ from . import models
 class ArmourPieceType(DjangoObjectType):
     class Meta:
         model = models.ArmourPiece
+
+
+class Query(graphene.ObjectType):
+    all_head_pieces = graphene.List(ArmourPieceType)
+    all_chest_pieces = graphene.List(ArmourPieceType)
+    all_arms_pieces = graphene.List(ArmourPieceType)
+    all_waist_pieces = graphene.List(ArmourPieceType)
+    all_legs_pieces = graphene.List(ArmourPieceType)
+
+    def resolve_all_head_pieces(root, info):
+        return models.ArmourPiece.objects.filter(
+            armour_type=models.ArmourPiece.ArmourType.HEAD
+        )
+
+    def resolve_all_chest_pieces(root, info):
+        return models.ArmourPiece.objects.filter(
+            armour_type=models.ArmourPiece.ArmourType.CHEST
+        )
+
+    def resolve_all_arms_pieces(root, info):
+        return models.ArmourPiece.objects.filter(
+            armour_type=models.ArmourPiece.ArmourType.ARMS
+        )
+
+    def resolve_all_waist_pieces(root, info):
+        return models.ArmourPiece.objects.filter(
+            armour_type=models.ArmourPiece.ArmourType.WAIST
+        )
+
+    def resolve_all_legs_pieces(root, info):
+        return models.ArmourPiece.objects.filter(
+            armour_type=models.ArmourPiece.ArmourType.LEGS
+        )
+
+
+schema = graphene.Schema(query=Query)
