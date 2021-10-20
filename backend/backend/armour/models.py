70118a8150922a590ee5224cdff0fb46e7c4b371
Jacob Howell <jhowell45@googlemail.com>
2021-10-20 19:35:42 +0100

    Creating table for sets.

--- backend/backend/armour/models.py
+++ backend/backend/armour/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 # Create your models here.
+class ArmourSet(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
 class ArmourPiece(models.Model):
     class ArmourType(models.IntegerChoices):
         HEAD = 0
@@ -11,7 +17,7 @@ class ArmourPiece(models.Model):
         LEGS = 4
 
     name = models.CharField(max_length=255, unique=True)
-    set_name = models.CharField(max_length=255)
+    set_name = models.ForeignKey(ArmourSet, on_delete=models.CASCADE)
     rarity = models.IntegerField()
     min_defence = models.IntegerField()
     max_defence = models.IntegerField(null=True, blank=True)
