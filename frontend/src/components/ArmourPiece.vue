5d6e38b854f063b85726265b6e547bb513ba2a37
Jacob Howell <jhowell45@googlemail.com>
2021-10-24 23:43:05 +0100

    Creating component.

---
+++ frontend/src/components/ArmourPiece.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link :to="`/armour-piece/${armourPiece.name}`">{{ displayName }}</router-link>
+</template>
+
+<script>
+export default ({
+    name: 'ArmourPiece',
+    props: {
+        armourPiece: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        displayName() {
+            return (
+                this.armourPiece.name
+            )
+        }
+    }
+})
+</script>
